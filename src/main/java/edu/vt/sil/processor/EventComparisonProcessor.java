73f808c653227ecaa3ae04db28a297f9b861e909
DedoCibula <dedo.cibula@gmail.com>
2016-02-10 02:22:16 -0500

    added OverlapProcessor, minor fixes

--- src/main/java/edu/vt/sil/processor/EventComparisonProcessor.java
+++ src/main/java/edu/vt/sil/processor/EventComparisonProcessor.java
@@ -6,7 +6,6 @@ import edu.vt.sil.entities.ExperimentResult;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
