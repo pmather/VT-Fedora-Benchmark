21504ccfbfc50064a0d71488352de36874ce8ac2
DedoCibula <dedo.cibula@gmail.com>
2016-02-17 20:44:13 -0500

    added rabbitmq backend code - workqueue processing

--- experiments/experiment1.py
+++ experiments/experiment1.py
@@ -1,142 +1,168 @@
 import datetime
 import boto3
+import pika
 import h5py
 import pycurl
-import sys,os
+import sys, os
 import time
 import xmltodict
 
 from socket import error as SocketError
 from subprocess import call
-from StringIO import StringIO    
+from StringIO import StringIO
+
+
+class RabbitMQClient(object):
+    def __init__(self, queuename):
+        super(RabbitMQClient, self).__init__()
+        self.queuename = queuename
+        self.connection = pika.BlockingConnection()
+        self.channel = self.connection.channel()
+        self.delivery_tag = None
+
+    def downloadWorkItem(self):
+        if self.delivery_tag:
+            self.channel.basic_ack(self.delivery_tag)
+        while True:
+            method_frame, header_frame, body = self.channel.basic_get(self.queuename)
+            if method_frame:
+                self.delivery_tag = method_frame.delivery_tag
+                if not body:
+                    self._disconnect()
+                return body
+
+    def _disconnect(self):
+        self.channel.basic_ack(self.delivery_tag)
+        self.channel.close()
+        self.connection.close()
+
 
 def downloadFromS3(filename):
-	s3 = boto3.client('s3')
-	s3.download_file('sebdata', filename, filename)
+    s3 = boto3.client('s3')
+    s3.download_file('sebdata', filename, filename)
+
 
 def downloadFromGDrive(gdrivedir, filename):
-	url = "https://googledrive.com/host/{}/{}".format(gdrivedir, filename)
-	call("wget " + url + " -O " + filename, shell=True)
+    url = "https://googledrive.com/host/{}/{}".format(gdrivedir, filename)
+    call("wget " + url + " -O " + filename, shell=True)
 
-def createFedoraObject(rdfdata, fedoraurl):	
-	storage = StringIO()
-	c = pycurl.Curl()
-	c.setopt(c.URL, fedoraurl)
-	c.setopt(c.POST, 1)
-	c.setopt(pycurl.HTTPHEADER, ["Content-type: text/turtle"])
-	c.setopt(c.POSTFIELDS, rdfdata)
-	c.setopt(c.WRITEFUNCTION, storage.write)
-	c.perform()
-	c.close()
-	content = storage.getvalue()
-	
-	return content
+
+def createFedoraObject(rdfdata, fedoraurl, filename):
+    storage = StringIO()
+    c = pycurl.Curl()
+    c.setopt(c.URL, "{}/{}".format(fedoraurl, filename[:-3]))
+    c.setopt(pycurl.CUSTOMREQUEST, "PUT")
+    c.setopt(pycurl.HTTPHEADER, ["Content-type: text/turtle"])
+    c.setopt(c.POSTFIELDS, rdfdata)
+    c.setopt(c.WRITEFUNCTION, storage.write)
+    c.perform()
+    c.close()
+    content = storage.getvalue()
+
+    return content
 
 
 # create fedora binary
 def createFedoraBinary(filepath, fedoraurl):
-	storage = StringIO()
-	f = open(filepath, "rb")
-	fs = os.path.getsize(filepath)
-	c = pycurl.Curl()
-	c.setopt(c.URL, fedoraurl)
-	c.setopt(c.PUT, 1)
-	c.setopt(c.READDATA, f)
-	c.setopt(c.INFILESIZE, int(fs))
-	c.setopt(pycurl.HTTPHEADER, ["Content-type: text/xml"])
-	c.setopt(c.WRITEFUNCTION, storage.write)
-	c.perform()
-	c.close()
-	content = storage.getvalue()
-	
-	return content
+    storage = StringIO()
+    f = open(filepath, "rb")
+    fs = os.path.getsize(filepath)
+    c = pycurl.Curl()
+    c.setopt(c.URL, fedoraurl)
+    c.setopt(c.PUT, 1)
+    c.setopt(c.READDATA, f)
+    c.setopt(c.INFILESIZE, int(fs))
+    c.setopt(pycurl.HTTPHEADER, ["Content-type: text/xml"])
+    c.setopt(c.WRITEFUNCTION, storage.write)
+    c.perform()
+    c.close()
+    content = storage.getvalue()
+
+    return content
 
 
-def main():
-	fedoraurl = sys.argv[1]
-	datasetfilename = sys.argv[2]
-	gdriveDir = sys.argv[3]
+def main(fedoraurl, queuename, gdriveDir):
+    outputfile = open("experiment1_{}_results.csv".format(datetime.date.today()), "a")
+    urlfile = open("fedoraurls.txt", "a")
 
-	outputfile = open("experiment1_{}_results.csv".format(datetime.date.today()), "a")
-	urlfile = open("fedoraurls.txt", "a")
+    progress = []
+    rabbitMq = RabbitMQClient(queuename)
 
-	progress = []
+    start = str(datetime.datetime.now())
+    tic = time.time()
 
-	start = str(datetime.datetime.now())
-	tic = time.time()
+    while True:
+        line = rabbitMq.downloadWorkItem()
+        if not line:
+            break
+        fileName = line.strip()
 
-	with open(datasetfilename) as f:
-		lines = f.readlines()
-	
-	for line in lines:
-		fileName = line.strip()
+        # downloadFromS3(fileName)
+        download = time.time()
+        downloadFromGDrive(gdriveDir, fileName)
+        progress.append("Download," + fileName + "," + str(download) + "," + str(time.time()))
 
-		# downloadFromS3(fileName)
-		download = time.time()
-		downloadFromGDrive(gdriveDir, fileName)
-		progress.append("Download," + fileName + "," + str(download) + "," + str(time.time()))
+        # read hdf5 file
+        f = h5py.File(fileName, 'r')
 
-		# read hdf5 file
-		f = h5py.File(fileName, 'r')
+        processing = time.time()
+        if f.keys()[0] is not None:
+            datasets = f[f.keys()[0]]
+            channel_str = ""
+            c = 0
+            for channel in datasets.keys():
+                if c == len(datasets.keys()) - 1:
+                    channel_str = channel_str + '<> dc:coverage "' + channel + '" '
+                else:
+                    channel_str = channel_str + '<> dc:coverage "' + channel + '" . '
+                c += 1
 
-		processing = time.time()
-		if f.keys()[0] is not None:
-			datasets = f[f.keys()[0]]
-			channel_str = ""
-			c = 0
-			for channel in datasets.keys():
-				if c == len(datasets.keys()) - 1:
-					channel_str = channel_str + '<> dc:coverage "'+ channel +'" '
-				else:
-					channel_str = channel_str + '<> dc:coverage "'+ channel +'" . '
-				c += 1
+            # run fits program
+            call("fits-0.9.0/fits.sh -i " + fileName + " > " + fileName + "_fits.xml", shell=True)
 
-			# run fits program 
-			call("fits-0.9.0/fits.sh -i " + fileName + " > " + fileName + "_fits.xml", shell=True)
+            # read fits xml
+            fitsxml = open(fileName + "_fits.xml", 'r').read()
+            result = xmltodict.parse(fitsxml)
+            description = result['fits']['identification']['identity'][0]['@format']
+            format = result['fits']['identification']['identity'][0]['@mimetype']
 
-			# read fits xml
-			fitsxml = open(fileName + "_fits.xml", 'r').read()
-			result = xmltodict.parse( fitsxml )
-			description = result['fits']['identification']['identity'][0]['@format']
-			format = result['fits']['identification']['identity'][0]['@mimetype']
+            fits_str = '<> dc:description "' + description + '" . ' + '<> dc:format "' + format + '" . '
 
-			fits_str = '<> dc:description "' + description + '" . ' + '<> dc:format "' + format + '" . '  		
+            # Create Fedora object
+            rdfdata = 'PREFIX dc: <http://purl.org/dc/elements/1.1/> <> dc:title "' + fileName + '" . ' + \
+                      fits_str + channel_str
+            progress.append("Processing," + fileName + "," + str(processing) + "," + str(time.time()))
 
-			# Create Fedora object
-			rdfdata = 'PREFIX dc: <http://purl.org/dc/elements/1.1/> <> dc:title "'+ fileName +'" . ' + \
-				fits_str + channel_str
-			progress.append("Processing," + fileName + "," + str(processing) + "," + str(time.time()))
+            ingestion = time.time()
+            objecturl = ""
+            for x in xrange(0, 5):
+                try:
+                    objecturl = createFedoraObject(rdfdata, fedoraurl, fileName)
+                    break
+                except SocketError as e:
+                    if x == 4:
+                        raise SocketError("retry 5 times still failed in fedora" + str(e.errno))
+                    pass
 
-			ingestion = time.time()
-			objecturl = ""
-			for x in xrange(0, 5):
-				try:
-					objecturl = createFedoraObject(rdfdata, fedoraurl)
-					break
-				except SocketError as e:
-					if x == 4:
-						raise SocketError("retry 5 times still failed in fedora" + str(e.errno))
-					pass
-			
-			# Create Fedora binary
-			if len(objecturl) > 0:
-				fileurl = objecturl + "/h5"
-				createFedoraBinary(fileName, fileurl)
-				progress.append("Ingestion," + fileName + "," + str(ingestion) + "," + str(time.time()))
-				urlfile.write(objecturl+"\n")
-				print fileurl
+            # Create Fedora binary
+            if len(objecturl) > 0:
+                fileurl = objecturl + "/h5"
+                createFedoraBinary(fileName, fileurl)
+                progress.append("Ingestion," + fileName + "," + str(ingestion) + "," + str(time.time()))
+                urlfile.write(objecturl + "\n")
+                print fileurl
 
-		os.remove(fileName)
-		os.remove(fileName + "_fits.xml")
+        os.remove(fileName)
+        os.remove(fileName + "_fits.xml")
 
-	duration = str(time.time() - tic)
-	end = str(datetime.datetime.now())
-	print duration
-	progress.insert(0, "OVERALL EXECUTION," + start + "," + duration + "," + end)
-	for line in progress:
-		outputfile.write(line + "\n")
-	outputfile.close()
-	urlfile.close()
+    duration = str(time.time() - tic)
+    end = str(datetime.datetime.now())
+    print duration
+    progress.insert(0, "OVERALL EXECUTION," + start + "," + duration + "," + end)
+    for line in progress:
+        outputfile.write(line + "\n")
+    outputfile.close()
+    urlfile.close()
 
 
-if __name__ == "__main__": main()
+if __name__ == "__main__": main(sys.argv[1], sys.argv[2], sys.argv[3])
--- experiments/experiment2.py
+++ experiments/experiment2.py
@@ -1,98 +1,128 @@
 import datetime
 import json
 import pycurl
+import pika
 import requests
-import sys,os
+import sys, os
 import time
 
 from subprocess import call
 from StringIO import StringIO
 
-def readFedoraObject(fedoraurl):	
-	storage = StringIO()
-	c = pycurl.Curl()
-	c.setopt(c.URL, fedoraurl)
-	c.setopt(pycurl.HTTPHEADER, ["Accept: application/ld+json"])
-	c.setopt(c.WRITEFUNCTION, storage.write)
-	c.perform()
-	c.close()
-	content = storage.getvalue()
-	
-	return content
+
+class RabbitMQClient(object):
+    def __init__(self, queuename):
+        super(RabbitMQClient, self).__init__()
+        self.queuename = queuename
+        self.connection = pika.BlockingConnection()
+        self.channel = self.connection.channel()
+        self.delivery_tag = None
+
+    def downloadWorkItem(self):
+        if self.delivery_tag:
+            self.channel.basic_ack(self.delivery_tag)
+        while True:
+            method_frame, header_frame, body = self.channel.basic_get(self.queuename)
+            if method_frame:
+                self.delivery_tag = method_frame.delivery_tag
+                if not body:
+                    self._disconnect()
+                return body
+
+    def _disconnect(self):
+        self.channel.basic_ack(self.delivery_tag)
+        self.channel.close()
+        self.connection.close()
+
+
+def readFedoraObject(fedoraurl):
+    storage = StringIO()
+    c = pycurl.Curl()
+    c.setopt(c.URL, fedoraurl)
+    c.setopt(pycurl.HTTPHEADER, ["Accept: application/ld+json"])
+    c.setopt(c.WRITEFUNCTION, storage.write)
+    c.perform()
+    c.close()
+    content = storage.getvalue()
+
+    return content
+
 
 def updateFedoraBinary(updatestr, fedoraurl):
-	binaryres = requests.patch(url=fedoraurl,
-						data=updatestr,
-						headers={'Content-Type': 'application/sparql-update'})
+    binaryres = requests.patch(url=fedoraurl,
+                               data=updatestr,
+                               headers={'Content-Type': 'application/sparql-update'})
 
-	return binaryres.text;
+    return binaryres.text
 
 
 def getFedoraSha(input):
-	json_data = json.loads( input )
-	sha_data = json_data[0]['http://www.loc.gov/premis/rdf/v1#hasMessageDigest'][0]['@id']
+    json_data = json.loads(input)
+    sha_data = json_data[0]['http://www.loc.gov/premis/rdf/v1#hasMessageDigest'][0]['@id']
 
-	return sha_data.replace('urn:sha1:', "")
+    return sha_data.replace('urn:sha1:', "")
+
 
 def sha1OfFile(filepath):
-	import hashlib
-	with open(filepath, 'rb') as f:
-		return hashlib.sha1(f.read()).hexdigest()
+    import hashlib
+    with open(filepath, 'rb') as f:
+        return hashlib.sha1(f.read()).hexdigest()
 
-def main():
-	fedorurls = sys.argv[1]
 
-	outputfile = open("experiment2_{}_results.csv".format(datetime.date.today()), "a")
+def main(queuename):
+    outputfile = open("experiment2_{}_results.csv".format(datetime.date.today()), "a")
 
-	progress = []
+    progress = []
+    rabbitMq = RabbitMQClient(queuename)
 
-	start = str(datetime.datetime.now())
-	tic = time.time()
+    start = str(datetime.datetime.now())
+    tic = time.time()
 
-	with open(fedorurls) as f:
-		lines = f.readlines()
+    fileName = "temp.h5"
+    while True:
+        line = rabbitMq.downloadWorkItem()
+        if not line:
+            break
+        fedoraobjurl = line.strip()
+        fedorah5url = fedoraobjurl + "/h5"
 
-	fileName = "temp.h5"
-	for line in lines:
-		fedoraobjurl = line.strip()
-		fedorah5url = fedoraobjurl + "/h5" 
-	
-		# read fedora object
-		processing = time.time()
-		content = readFedoraObject(fedorah5url + "/fcr:metadata")
+        # read fedora object
+        processing = time.time()
+        content = readFedoraObject(fedorah5url + "/fcr:metadata")
 
-		# read fedora sha
-		fedora_sha = getFedoraSha(content)
+        # read fedora sha
+        fedora_sha = getFedoraSha(content)
 
-		# download h5 file 
-		download = time.time()
-		call("wget " + fedorah5url + " -O " + fileName, shell=True)
-		downloadelapsed = time.time() - download
-		progress.append("Download," + fedoraobjurl + "," + str(download) + "," + str(download + downloadelapsed))
+        # download h5 file
+        download = time.time()
+        call("wget " + fedorah5url + " -O " + fileName, shell=True)
+        downloadelapsed = time.time() - download
+        progress.append("Download," + fedoraobjurl + "," + str(download) + "," + str(download + downloadelapsed))
 
-		# create sha-1
-		file_sha = sha1OfFile(fileName)
+        # create sha-1
+        file_sha = sha1OfFile(fileName)
 
-		# compare sha-1 and update fedora object
-		if fedora_sha == file_sha:
-			sharesult = "digest check passed at " + datetime.datetime.utcnow().isoformat() + 'Z' 
-		else:
-			sharesult = "digest check failed at " + datetime.datetime.utcnow().isoformat() + 'Z'
-		
-		updatestr = "PREFIX dc: <http://purl.org/dc/elements/1.1/> INSERT { <> dc:provenance \"" + sharesult + "\" . } WHERE { } "
-		progress.append("Processing," + fedoraobjurl + "," + str(processing) + "," + str(time.time() - downloadelapsed))
+        # compare sha-1 and update fedora object
+        if fedora_sha == file_sha:
+            sharesult = "digest check passed at " + datetime.datetime.utcnow().isoformat() + 'Z'
+        else:
+            sharesult = "digest check failed at " + datetime.datetime.utcnow().isoformat() + 'Z'
 
-		ingestion = time.time()
-		updateFedoraBinary(updatestr, fedoraobjurl)
-		progress.append("Ingestion," + fedoraobjurl + "," + str(ingestion) + "," + str(time.time()))
-		os.remove(fileName)
+        updatestr = "PREFIX dc: <http://purl.org/dc/elements/1.1/> INSERT { <> dc:provenance \"" + sharesult + "\" . } WHERE { } "
+        progress.append("Processing," + fedoraobjurl + "," + str(processing) + "," + str(time.time() - downloadelapsed))
 
-	duration = str(time.time() - tic)
-	end = str(datetime.datetime.now())
-	print duration
-	progress.insert(0, "OVERALL EXECUTION," + start + "," + duration + "," + end)
-	for line in progress:
-		outputfile.write(line + "\n")
-	outputfile.close()
+        ingestion = time.time()
+        updateFedoraBinary(updatestr, fedoraobjurl)
+        progress.append("Ingestion," + fedoraobjurl + "," + str(ingestion) + "," + str(time.time()))
+        os.remove(fileName)
 
-if __name__ == "__main__": main()
+    duration = str(time.time() - tic)
+    end = str(datetime.datetime.now())
+    print duration
+    progress.insert(0, "OVERALL EXECUTION," + start + "," + duration + "," + end)
+    for line in progress:
+        outputfile.write(line + "\n")
+    outputfile.close()
+
+
+if __name__ == "__main__": main(sys.argv[1])
