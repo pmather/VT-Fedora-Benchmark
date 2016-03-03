3c0715913c165327ff233e021e7f5a43df2f6a51
DedoCibula <dedo.cibula@gmail.com>
2016-03-03 01:12:39 -0500

    small fixes in names

--- user_scripts/client_bootstrap.sh
+++ user_scripts/client_bootstrap.sh
@@ -4,7 +4,7 @@ git clone https://DedoCibula@bitbucket.org/DedoCibula/vt-sil.git
 vt-sil/utils/client_setup.sh
 
 echo_supervisord_conf > /etc/supervisord.conf
-echo "[program:rabbitmq_consumer]" >> /etc/supervisord.conf
+echo "[program:experiment_coordinator]" >> /etc/supervisord.conf
 echo "command=nice -n -5 python experiment_coordinator.py <rabbitmq_url> admin admin" >> /etc/supervisord.conf
 echo "directory=${PWD}/vt-sil/experiments" >> /etc/supervisord.conf
 echo "redirect_stderr=true" >> /etc/supervisord.conf
