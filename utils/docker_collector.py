fcbac9e9b11b1937711d94ecb1e63e1d3a28f7eb
DedoCibula <dedo.cibula@gmail.com>
2016-03-08 23:26:19 -0500

    reverted to base package

--- utils/docker_collector.py
+++ utils/docker_collector.py
@@ -1,21 +1,21 @@
-from os import path, listdir, devnull
+from os
 from subprocess import call
 
 
 def main():
-    base_path = path.join(path.dirname(path.dirname(path.realpath(__file__))), "experiments")
-    for file in listdir(base_path):
-        file_path = path.join(base_path, file)
+    base_path = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "experiments")
+    for file in os.listdir(base_path):
+        file_path = os.path.join(base_path, file)
         try:
-            if path.isfile(file_path):
+            if os.path.isfile(file_path):
                 os.unlink(file_path)
         except Exception, e:
             print e
     call("docker cp fedora_benchmark:/vt-fedora-benchmark/experiments/. {}".format(base_path), shell=True)
-    with open(path.join(base_path, "experiment.out"), "w") as f, open(devnull, 'w') as fnull:
+    with open(os.path.join(base_path, "experiment.out"), "w") as f, open(os.devnull, 'w') as fnull:
         call(["docker", "logs", "fedora_benchmark"], stdout=f, stderr=fnull)
-    for file in listdir(base_path):
-        print path.join(base_path, file)
+    for file in os.listdir(base_path):
+        print os.path.join(base_path, file)
 
 
 if __name__ == "__main__": main()
