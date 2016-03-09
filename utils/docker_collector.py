448f0aec3f0e5fc8096674575a4273750b121701
DedoCibula <dedo.cibula@gmail.com>
2016-03-08 23:22:17 -0500

    added cleanup before fetching of results

--- utils/docker_collector.py
+++ utils/docker_collector.py
@@ -4,6 +4,13 @@ from subprocess import call
 
 def main():
     base_path = path.join(path.dirname(path.dirname(path.realpath(__file__))), "experiments")
+    for file in listdir(base_path):
+        file_path = path.join(base_path, file)
+        try:
+            if path.isfile(file_path):
+                os.unlink(file_path)
+        except Exception, e:
+            print e
     call("docker cp fedora_benchmark:/vt-fedora-benchmark/experiments/. {}".format(base_path), shell=True)
     with open(path.join(base_path, "experiment.out"), "w") as f, open(devnull, 'w') as fnull:
         call(["docker", "logs", "fedora_benchmark"], stdout=f, stderr=fnull)
