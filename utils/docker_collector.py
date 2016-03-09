4198fe841912399ba11fcb4f73582dfb212a9a4a
DedoCibula <dedo.cibula@gmail.com>
2016-03-08 21:28:04 -0500

    added sudo to command execution and modified docker_collector

--- utils/docker_collector.py
+++ utils/docker_collector.py
@@ -1,11 +1,12 @@
-from os import path, listdir
+from os import path, listdir, devnull
 from subprocess import call
 
 
 def main():
     base_path = path.join(path.dirname(path.dirname(path.realpath(__file__))), "experiments")
     call("docker cp fedora_benchmark:/vt-fedora-benchmark/experiments/. {}".format(base_path), shell=True)
-    call("docker logs fedora_benchmark > {}/experiment.out".format(base_path), shell=True)
+    with open(path.join(base_path, "experiment.out"), "w") as f, open(devnull, 'w') as fnull:
+        call(["docker", "logs", "fedora_benchmark"], stdout=f, stderr=fnull)
     for file in listdir(base_path):
         print path.join(base_path, file)
 
