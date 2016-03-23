6df13ee89e631bcefa630f4c8e2e37aa472060a5
DedoCibula <dedo.cibula@gmail.com>
2016-03-23 00:09:22 -0400

    fixes in supervisor code

--- user_scripts/docker/client_with_rabbitmq_bootstrap.sh
+++ user_scripts/docker/client_with_rabbitmq_bootstrap.sh
@@ -20,5 +20,5 @@ sudo service ntp restart
 docker run -d -p 5672:5672 -p 15672:15672  --hostname ${RABBITMQ_URL} --name ${RABBITMQ_URL} -e RABBITMQ_DEFAULT_USER=${RABBITMQ_USERNAME} -e RABBITMQ_DEFAULT_PASS=${RABBITMQ_PASSWORD} rabbitmq:management
 
 for ((i = 1; i <= ${THREADS}; i++)) ; do
-    docker run -d --privileged --link=${RABBITMQ_URL}:${RABBITMQ_URL} --name=fedora_benchmark dedocibula/fedora-benchmark python experiment_coordinator.py ${RABBITMQ_URL} ${RABBITMQ_USERNAME} ${RABBITMQ_PASSWORD}
+    docker run -d --privileged --link=${RABBITMQ_URL}:${RABBITMQ_URL} --name=fedora_benchmark_${i} dedocibula/fedora-benchmark python experiment_coordinator.py ${RABBITMQ_URL} ${RABBITMQ_USERNAME} ${RABBITMQ_PASSWORD}
 done
--- user_scripts/vm/client_with_rabbitmq_bootstrap.sh
+++ user_scripts/vm/client_with_rabbitmq_bootstrap.sh
@@ -25,11 +25,11 @@ git clone https://DedoCibula@bitbucket.org/DedoCibula/vt-fedora-benchmark.git
 vt-fedora-benchmark/utils/client_setup.sh
 ln -s vt-fedora-benchmark/utils/vm_collector.py collector.py
 
+echo_supervisord_conf > /etc/supervisord.conf
 for ((i = 1; i <= ${THREADS}; i++)) ; do
     mkdir ${PWD}/vt-fedora-benchmark/${i}
-    cp -R ${PWD}/vt-fedora-benchmark/experiments ${PWD}/vt-fedora-benchmark/${i}
+    cp -R ${PWD}/vt-fedora-benchmark/experiments/* ${PWD}/vt-fedora-benchmark/${i}
 
-    echo_supervisord_conf > /etc/supervisord.conf
     echo "[program:experiment_coordinator_${i}]" >> /etc/supervisord.conf
     echo "command=nice -n -5 python experiment_coordinator.py ${RABBITMQ_URL} ${RABBITMQ_USERNAME} ${RABBITMQ_PASSWORD}" >> /etc/supervisord.conf
     echo "directory=${PWD}/vt-fedora-benchmark/${i}" >> /etc/supervisord.conf
