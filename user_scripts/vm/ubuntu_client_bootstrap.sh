0f1f1751fa5d99f612f38c48614b4bfd554552bc
Paul Mather <pmather@vt.edu>
2016-05-11 11:31:55 -0400

    Make GitHub VTUL/VT-Fedora-Benchmark the canonical repository
    Alter "git clone" checkouts of repository so they fetch from the VTUL
    repository, not the personal repository hosted on BitBucket.

--- user_scripts/vm/ubuntu_client_bootstrap.sh
+++ user_scripts/vm/ubuntu_client_bootstrap.sh
@@ -4,7 +4,7 @@ RABBITMQ_USERNAME="admin"
 RABBITMQ_PASSWORD="admin"
 
 sudo apt-get upgrade && sudo apt-get update && sudo apt-get install -y git
-git clone https://DedoCibula@bitbucket.org/DedoCibula/vt-fedora-benchmark.git
+git clone https://github.com/VTUL/VT-Fedora-Benchmark.git vt-fedora-benchmark
 vt-fedora-benchmark/utils/client_setup.sh
 ln -s vt-fedora-benchmark/orchestrators/process_orchestrator.py collector.py
 
